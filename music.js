0919fdebd293c98ba74abdadbd059e0280197c67
Pat Kelly <PatK80@gmail.com>
2021-11-16 18:34:16 -0500

    Adding music.js file and all AL songs

---
+++ music.js
@@ -0,0 +1,42 @@
+const musicArray = [
+    {
+        albumName: "AL",
+        albumArtworkSrc: "img/2017-al.jpg",
+        albumYear: 2017,
+        artistName: "The Dead Flowers",
+        songFileSrc: "music/AL/drivin-on-down-to-tennessee.mp3",
+        songName: "Drivin' On Down To Tennessee"
+    },
+    {
+        albumName: "AL",
+        albumArtworkSrc: "img/2017-al.jpg",
+        albumYear: 2017,
+        artistName: "The Dead Flowers",
+        songFileSrc: "music/AL/whats-your-name.mp3",
+        songName: "What's Your Name"
+    },
+    {
+        albumName: "AL",
+        albumArtworkSrc: "img/2017-al.jpg",
+        albumYear: 2017,
+        artistName: "The Dead Flowers",
+        songFileSrc: "music/AL/daisy-38.mp3",
+        songName: "Daisy 38"
+    },
+    {
+        albumName: "AL",
+        albumArtworkSrc: "img/2017-al.jpg",
+        albumYear: 2017,
+        artistName: "The Dead Flowers",
+        songFileSrc: "music/AL/leather-dress.mp3",
+        songName: "Leather Dress"
+    },
+    {
+        albumName: "AL",
+        albumArtworkSrc: "img/2017-al.jpg",
+        albumYear: 2017,
+        artistName: "The Dead Flowers",
+        songFileSrc: "music/AL/c.c..mp3",
+        songName: "C.C."
+    },
+];
