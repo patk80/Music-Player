3509d09bd15616864c81e58fb4eeeebfb4ec7594
Pat Kelly <PatK80@gmail.com>
2021-11-17 19:21:14 -0500

    Adding next button in JS/html

--- scripts.js
+++ scripts.js
@@ -2,10 +2,16 @@ let albumArtwork  = document.querySelector("#album-artwork");
 let songName      = document.querySelector("#song-name");
 let artistName    = document.querySelector("#artist-name");
 let musicSource   = document.querySelector("#music-source");
+let nextButton    = document.querySelector("#next-button");
 
 function randomSong() {
     let randomIndex      = Math.floor( Math.random() * musicArray.length );
     albumArtwork.setAttribute("src", musicArray[randomIndex].albumArtworkSrc);
     songName.innerText   = musicArray[randomIndex].songName;
     artistName.innerText = musicArray[randomIndex].artistName;
+    musicSource.setAttribute("src", musicArray[randomIndex].songFileSrc);
 }
+
+nextButton.addEventListener("click", function(){
+    randomSong();
+});
