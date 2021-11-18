e3ff0a51e5e3238b0c4ab01f940085dd664eeaed
Pat Kelly <PatK80@gmail.com>
2021-11-17 19:29:59 -0500

    Comments in JS file for randomSong func

--- scripts.js
+++ scripts.js
@@ -6,7 +6,9 @@ let nextButton         = document.querySelector("#next-button");
 let audioElement       = document.querySelector("audio");
 
 function randomSong() {
+    // Generate random index number //
     let randomIndex      = Math.floor( Math.random() * musicArray.length );
+    // Use that random index number to pick a random song Object from music.js and update the HTML with those properties //
     albumArtwork.setAttribute("src", musicArray[randomIndex].albumArtworkSrc);
     songName.innerText   = musicArray[randomIndex].songName;
     artistName.innerText = musicArray[randomIndex].artistName;
